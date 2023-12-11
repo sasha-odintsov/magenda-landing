d706d3ea984d8cb307af03b50d5082ef3c7415c3
sasha-odintsov <aleks-one@ukr.net>
2023-12-11 16:51:07 +0200

    Added sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,24 @@ module.exports = {
       fontFamily: {
         "montserrat": ["Montserrat", "sans-serif"],
       },
+      screens: {
+        "3xl": "1921px",
+      },
       colors: {
         "blue": {
           "main": "#2B579A",
           "dark": "#173D77",
           "light": "#F5F9FF",
           "contrast": "#5E7ED3",
+          "middle": "#97C3F9",
+          "light-contrast": "#DFE9FD",
         },
         "gray": {
           DEFAULT: "#7D7D7D",
           100: "#E0E0E0",
+          "middle": "#575757",
         },
+        dark: "#0F172A",
       },
       boxShadow: {
         "around": "0px 0px 15px 5px rgba(43,87,154,0.1);"
