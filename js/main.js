7d5c6309740855298823ee86ca6a4b483e30be03
sasha-odintsov <aleks-one@ukr.net>
2023-12-15 15:27:16 +0200

    Added swiper

--- js/main.js
+++ js/main.js
@@ -14,4 +14,32 @@ $(() => {
   setHeight();
 
   $(window).resize(setHeight);
+
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    slidesPerView: 3.5,
+    slidesPerGroup: 3,
+    spaceBetween: 25,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: (index, className) => {
+        return `<span class=${className}>0${index + 1}</span>`;
+      },
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2.5,
+        slidesPerGroup: 2,
+      },
+      1280: {
+        slidesPerView: 3.5,
+        slidesPerGroup: 3,
+      },
+      1536: {
+        spaceBetween: 50,
+      },
+    },
+  });
 });
